e85d83467c62e838bbaa263e4f57c3850de3fc24
Chris <chrisgai01@icloud.com>
2023-10-04 09:27:16 -0500

    warmup

---
+++ WarmUp10423/src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        String personName = "John Doe";
+        int personAge = 25;
+        double productPrice = 19.99;
+        float temperatureCelsius = 27.5f;
+        boolean isLoggedIn = true;
+
+        
+
+
+
+    }
+}
