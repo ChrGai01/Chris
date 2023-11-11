0e522c6b9e22a604e9d5fbd5bfe88578511a340f
Chris <chrisgai01@icloud.com>
2023-11-11 10:03:36 -0600

    nw

---
+++ Week 5 Project/VehicleDealership/src/main/java/org/example/SalesContract.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class SalesContract {
+    boolean isFinanced;
+
+    public SalesContract(boolean isFinanced) {
+        this.isFinanced = isFinanced;
+    }
+
+    public boolean isFinanced() {
+        return isFinanced;
+    }
+
+    public void setFinanced(boolean financed) {
+        isFinanced = financed;
+    }
+    double getSalesTaxAmount(){
+        return 
+    }
+}
+
